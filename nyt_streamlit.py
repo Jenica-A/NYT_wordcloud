d9d67af84023a06bb4d703c6b2375f12ae20a9f1
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-08-06 17:24:43 -0600

    Update nyt_streamlit.py

--- nyt_streamlit.py
+++ nyt_streamlit.py
@@ -49,8 +49,8 @@ st.pyplot(plt)
 
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
-start_date = st.date_input('Start date', today)
-end_date = st.date_input('End date', tomorrow)
+start_date = st.date_input('Start date', min(df.pub_date))
+end_date = st.date_input('End date', max(df.pub_date))
 if start_date < end_date:
     st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
 else:
