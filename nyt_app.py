2c669428748a2f7540eb952c90865f1f03b0d275
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-08-06 20:18:52 -0600

    Update nyt_app.py

--- nyt_app.py
+++ nyt_app.py
@@ -69,7 +69,7 @@ def black_color_func(word, font_size, position,orientation,random_state=None, **
 
 #@st.cache
 def wordcloud_func(text):
-    word_cloud = WordCloud(collocations = False, background_color = 'white',width=3000, height=2000, max_words=150, color_func = black_color_func).generate(text)
+    word_cloud = WordCloud(collocations = False, background_color = 'white',width=3000, height=2000, max_words=200, color_func = black_color_func).generate(text)
 
     # Display the generated image:
     plt.imshow(word_cloud, interpolation='bilinear')
