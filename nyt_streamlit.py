8b5a08a4196f4017bb106b268ac81b4073d627c2
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-08-06 19:05:39 -0600

    Update nyt_streamlit.py

--- nyt_streamlit.py
+++ nyt_streamlit.py
@@ -13,8 +13,10 @@ from wordcloud import WordCloud
 import datetime
 
 
-st.title("New York Times Topics")
+st.title("New York Times Topic Modelling")
 st.write("'More information is always better than less. When people know the reason things are happening, even if it's bad news, they can adjust their expectations and react accordingly. Keeping people in the dark only serves to stir negative emotions.' \n\n — Simon Sinek")
+
+st.text(/n"This app will allow you to choose a range of dates, and a section from the New York Times and generate a wordcloud image, based on word frequency wihtin article snippets")
 
 data_path = ("./df_snip_filtered.csv")
 
