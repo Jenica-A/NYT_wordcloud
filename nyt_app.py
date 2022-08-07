2ccd8634962fd704ea1769f5f53476ca4556d863
Jenica-A <95931276+Jenica-A@users.noreply.github.com>
2022-08-06 19:49:09 -0600

    Update nyt_app.py

--- nyt_app.py
+++ nyt_app.py
@@ -53,11 +53,10 @@ st.write(selection)
 #st.write('You selected:', selection)
 
 #filtered_data = df[df['pub_date'] == start_date]
-st.subheader(f"wordcloud of words in {start_date}")
+st.subheader(f"wordcloud of words in articles from {start_date} to {end_date} in the {selection} section(s)")
 
 filtered_data = df[(df['pub_date'] > pd.Timestamp(start_date)) & (df['pub_date'] < pd.Timestamp(end_date))]
-#rslt_df = dataframe[dataframe['Stream'].isin(options)] 
-filtered_data = filtered_data[(filtered_data.section_name.isin(selection))]
+filtered_data = filtered_data[(filtered_data['section_name'].isin(selection))]
 text = " ".join(word for word in filtered_data.filtered)
 
 #generate text from filtered column of df
