a1883dd72aa4d3391f16d31e0a4098c7c77027ec
Jenica Andersen <95931276+Jenica-A@users.noreply.github.com>
2024-07-26 16:23:49 -0600

    Update nyt_streamlit.py

--- nyt_streamlit.py
+++ nyt_streamlit.py
@@ -14,8 +14,6 @@ import datetime
 
 
 st.title("New York Times Topic Modelling")
-st.write("'More information is always better than less. When people know the reason things are happening, even if it's bad news, they can adjust their expectations and react accordingly. Keeping people in the dark only serves to stir negative emotions.' \n\n — Simon Sinek")
-
 st.text("This app will allow you to choose a range of dates, and a section from the New York Times and generate a wordcloud image, based on word frequency wihtin article excerpts")
 
 data_path = ("./df_snip_filtered.csv")
@@ -75,6 +73,7 @@ def wordcloud_func(text):
 
 wordcloud_func(text)
 
+st.write("'More information is always better than less. When people know the reason things are happening, even if it's bad news, they can adjust their expectations and react accordingly. Keeping people in the dark only serves to stir negative emotions.' \n\n — Simon Sinek")
 
 # Notes:
 # - Switch from function to procedural
